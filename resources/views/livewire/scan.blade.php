a3a594898ce4e3dc2e5eec8848f8837e0ad16fb4
itok toni <itok.toni@gmail.com>
2026-05-15 13:56:53 +0700

    update

--- resources/views/livewire/scan.blade.php
+++ resources/views/livewire/scan.blade.php
@@ -21,7 +21,7 @@
             </div>
         @endif
 
-        @if(!empty($error) && ($error != 'Success Create Record !' || $error != 'Success Update Record !'))
+        @if(!empty($error) && ($error == 'Success Create Record !' || $error == 'Success Update Record !'))
          <div class="alert alert-success mb-0">
                 <i class="fas fa-check-circle"></i> <strong>Success :</strong> {{ $error }}
             </div>
